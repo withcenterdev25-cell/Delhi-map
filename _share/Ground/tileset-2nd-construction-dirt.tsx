42de820cb0eb1bb968345b8a109c86ecaabcd856
withcenterdev25-cell <321302249+withcenterdev25-cell@users.noreply.github.com>
2026-08-28 09:27:53 +0800

    Added construction site

--- _share/Ground/tileset-2nd-construction-dirt.tsx
+++ _share/Ground/tileset-2nd-construction-dirt.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="tileset" tilewidth="64" tileheight="32" tilecount="1449" columns="54">
+<tileset version="1.10" tiledversion="1.10.2" name="tileset-2nd-construction-dirt" tilewidth="64" tileheight="32" tilecount="1458" columns="54">
  <image source="tileset-2nd-construction-dirt.png" width="3456" height="864"/>
 </tileset>
--- _share/Ground/tileset-pedestrian.tsx
+++ _share/Ground/tileset-pedestrian.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="tileset" tilewidth="64" tileheight="32" tilecount="2176" columns="65">
+<tileset version="1.10" tiledversion="1.10.2" name="tileset-pedestrian" tilewidth="64" tileheight="32" tilecount="2210" columns="65">
  <image source="tileset-pedestrian.png" width="4160" height="1088"/>
 </tileset>
